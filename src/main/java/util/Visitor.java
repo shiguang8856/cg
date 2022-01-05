c40dd6ff9288f3e7012c0c4ee315c424ac1daa2e
shiguang8856 <59004034+shiguang8856@users.noreply.github.com>
2022-01-05 10:37:37 +0800

    Delete src/main/java directory

--- src/main/java/util/Visitor.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public interface Visitor<T> {
-    /**
-     * @return {@code true} if the algorithm should visit more results,
-     * {@code false} if it should terminate now.
-     */
-    public boolean visit(T t);
-}
